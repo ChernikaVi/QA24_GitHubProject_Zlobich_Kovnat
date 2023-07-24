0696940dc6f40e6609083da415985d94d3406698
ChernikaVi <vzlobich6@gmail.com>
2023-07-24 16:45:54 +0300

    Pages,Tests,pom

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,64 @@
+package tests;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
+import pages.*;
+import utils.InvokedListener;
+import utils.PropertyReader;
+
+import java.time.Duration;
+
+@Listeners({InvokedListener.class})
+public abstract class BaseTest {
+    protected static final String BASE_URL = PropertyReader.getProperty("base_url");
+    protected static final String USERNAME = PropertyReader.getProperty("username");
+    protected static final String PASSWORD = PropertyReader.getProperty("password");
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected SignInPage signInPage;
+    protected LoginPage loginPage;
+    protected DashboardPage dashboardPage;
+    protected CreateNewRepositoryPage createNewRepositoryPage;
+
+
+    @Parameters({"browserName"})
+    @BeforeClass(alwaysRun = true)
+    public void setUp(@Optional("chrome") String browserName, ITestContext context) throws Exception {
+        if (browserName.equals("chrome")) {
+            driver = new ChromeDriver();
+        } else if (browserName.equals("firefox")) {
+            driver = new SafariDriver();
+        } else {
+            throw new Exception("Unsupported browser");
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        context.setAttribute("driver", driver);
+        signInPage = new SignInPage(driver);
+        loginPage = new LoginPage(driver);
+        dashboardPage = new DashboardPage(driver);
+        createNewRepositoryPage = new CreateNewRepositoryPage(driver);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+    }
+
+    @BeforeMethod(alwaysRun = true)
+    public void navigate() {
+        driver.get(BASE_URL);
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void clearCookies() {
+        driver.manage().deleteAllCookies();
+        ((JavascriptExecutor) driver).executeScript(String.format("window.localStorage.clear();"));
+        ((JavascriptExecutor) driver).executeScript(String.format("window.sessionStorage.clear();"));
+    }
+}
